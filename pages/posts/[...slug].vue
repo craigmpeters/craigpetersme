529f31076d9dfe8176aceb31becf1e506ad0006c
craigpeters@me.com <craigpeters@me.com>
2025-02-23 22:49:14 +0000

    typography fixes

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        <h2 class="text-xl pb-2 pt-1 text-orange-500">{{ post.title }}</h2>
-        <ContentRenderer v-if="post" :value="post" :excerpt=false tag="article"  />
+        <nav class="text-sm">
+            <NuxtLink to="/">Back</NuxtLink>
+        </nav>
+        <h2 class="text-3xl pb-2 pt-1 text-orange-500">{{ post.title }}</h2>
+        <ContentRenderer v-if="post" :value="post" :excerpt=false tag="article" class="space-y-4 text-"  />
     </div>
 </template>
 
@@ -16,8 +19,5 @@
 </script>
 
 <style scoped>
-    article p {
-        margin-bottom: 0.5rem;
-    }
 
 </style>
