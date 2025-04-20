069b5725fbba68181a355a90b212dfb5cfea90ac
craigpeters@me.com <craigpeters@me.com>
2025-04-20 11:21:02 +0100

    change font to Lexend

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <div id="articles">
             <ul>
                 <li v-for="post of posts" :key="post.path">
-                    <h2 class="text-xl pb-2 pt-1 text-orange-500"><NuxtLink :to="post.path">{{ post.title }}</NuxtLink></h2>
+                    <h2 class="text-xl pb-2 pt-3 text-orange-500"><NuxtLink :to="post.path">{{ post.title }}</NuxtLink></h2>
                     <ContentRenderer v-if="post" :value="post" :excerpt=true />
                 </li>
             </ul>
